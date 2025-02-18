beca4216832dfa5647d12095d1fbdb4a75f3d979
Sam Ko <samk3180@gmail.com>
2025-02-18 12:10:43 -0800

    installed nextjs, threejs, and materialui

---
+++ portfolio/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <>
+    <p>Hi! I'm Sam</p>
+    <Typography>Hi! I'm Sam</Typography>
+    </>
+  )
+}
