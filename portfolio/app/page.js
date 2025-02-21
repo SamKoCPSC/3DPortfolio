020af9902a407b13046550cf09c199e589e50538
Sam Ko <samk3180@gmail.com>
2025-02-21 14:33:11 -0800

    added blender model to page

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,15 +1,23 @@
 'use client'
-import styles from "./page.module.css";
-import { Typography } from "@mui/material";
-import { Canvas } from "@react-three/fiber";
+import styles from "./page.module.css"
+import { Typography } from "@mui/material"
+import { Canvas, useLoader } from "@react-three/fiber"
+import { Suspense } from "react"
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { OrbitControls, ScrollControls } from '@react-three/drei'
 
 export default function Home() {
+  const gltf = useLoader(GLTFLoader, '/ThreeJSPortfolioOptimized.glb')
+
   return (
-    <Canvas>
-      <mesh>
-        <boxGeometry/>
-        <meshStandardMaterial/>
-      </mesh>
-    </Canvas>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas camera={{fov: 60, position: [0, 5, 3], rotation: [0, 0, 0]}}>
+        <Suspense fallback={null}>
+          <ambientLight/>
+          {/* <OrbitControls/> */}
+          <primitive object={gltf.scene}/>
+        </Suspense>
+      </Canvas>
+    </div>
   )
 }
